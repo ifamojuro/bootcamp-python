2803cc85edb6f7c3dd6be3be9e1ef6ee78d2b75d
Ife Fahm <Ife Fahm>
2016-10-28 02:46:13 -0400

    finished hello world example

--- exercises-hello/hello.py
+++ exercises-hello/hello.py
@@ -2,10 +2,10 @@
 #
 # This is your first python program!
 # print out "hello world" to the terminal.
-#
 # When you are done, run the program with: python hello.py
 #
 # Run ./test.sh to make sure your program matches our expected output.
 #
 # TODO: write your code below
-
+print("hello world")
+#
